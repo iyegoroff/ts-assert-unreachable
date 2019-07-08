cbcaad42a2d5f8edb25f79417d2f6128bdd7c284
Igor Yegorov <iegoroff@gmail.com>
2019-07-09 02:35:20 +0300

    removed index.js

--- src/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var tiny_invariant_1 = require("tiny-invariant");
-exports.assertUnreachable = function (_, message) {
-    if (message === void 0) { message = 'unreachable'; }
-    return tiny_invariant_1["default"](false, message);
-};
