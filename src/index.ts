c9b4adb883d8248ae1cecf400f08983a81a56c35
Igor Yegorov <iegoroff@gmail.com>
2019-11-02 23:01:14 +0300

    upd

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import invariant from 'ts-tiny-invariant'
 
 export function unreachableCase(_: never, message = 'unreachable') {
-  return invariant(false, message)
+  return invariant(false, message) as never
 }
 
 export function unreachable(message = 'unreachable') {
-  return invariant(false, message)
+  return invariant(false, message) as never
 }
